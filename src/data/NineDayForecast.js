2e16b68be5b27f23669b87604e9f65a4c20ccae4
ddemonbay <77119766+ddemonbay@users.noreply.github.com>
2021-07-15 10:52:32 +0800

    added forecast features

--- src/data/NineDayForecast.js
+++ src/data/NineDayForecast.js
@@ -1,71 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { Line } from 'react-chartjs-2'
+
+
+const data = {
+    labels: ["","","","","","",""],
+    datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+    }]
+}
 
 export default function NineDayForecast({promise}) {
     let el
-    const [state, setState] = useState({
-        1: {
-            maxrh: 0,
-            maxtemp: 0,
-            minrh: 0,
-            mintemp: 0
-        },
-        2: {
-            maxrh: 0,
-            maxtemp: 0,
-            minrh: 0,
-            mintemp: 0
-        },
-        3: {
-            maxrh: 0,
-            maxtemp: 0,
-            minrh: 0,
-            mintemp: 0
-        },
-        4: {
-            maxrh: 0,
-            maxtemp: 0,
-            minrh: 0,
-            mintemp: 0
-        },
-        5: {
-            maxrh: 0,
-            maxtemp: 0,
-            minrh: 0,
-            mintemp: 0
-        },
-        6: {
-            maxrh: 0,
-            maxtemp: 0,
-            minrh: 0,
-            mintemp: 0
-        },
-        7: {
-            maxrh: 0,
-            maxtemp: 0,
-            minrh: 0,
-            mintemp: 0
-        },
-        8: {
-            maxrh: 0,
-            maxtemp: 0,
-            minrh: 0,
-            mintemp: 0
-        },
-        9: {
-            maxrh: 0,
-            maxtemp: 0,
-            minrh: 0,
-            mintemp: 0
-        },
-    })
+    // promise.then(data => {console.log(data)})
+    const [state, setState] = useState([])
+    // maxrh: 0, maxtemp: 0,minrh: 0, mintemp: 0
+
+    useEffect(() => {
+        promise.then(data => {
+            let days = []
+            for (let day of data) {
+                console.log(day)
+                days = [...days, {
+                    date: day.forecastDate,
+                    week: day.week,
+                    forecastWeather: day.forecastWeather,
+                    icon: day.ForecastIcon,
+                    maxrh: day.forecastMaxrh.value,
+                    minrh: day.forecastMinrh.value,
+                    maxtemp: day.forecastMaxtemp.value,
+                    mintemp: day.forecastMintemp.value,
+                }]
+            }
+            setState(days)
+        })
+    }, [])
 
     return (
         <div>
-            {/* {promise.then(data => {
-                for (el of data) {
-                    console.log(el.forecastMaxrh.value)
-                }
-            })} */}
+            <Line data={data} />
+            {state[1].mintemp}
             No Forecast Details
         </div>
     )
